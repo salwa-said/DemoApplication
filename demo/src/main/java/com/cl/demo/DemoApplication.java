56332e6f72836019f1176678d55481c440cff1e4
salwa-said <salwwa2000@gmail.com>
2026-08-24 10:50:17 +0400

    DemoApplicationTest

---
+++ demo/src/main/java/com/cl/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.cl.demo;
+
+import com.cl.demo.entities.Person;
+import com.cl.demo.entities.Task;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static List<Person> personList= new ArrayList<>();
+	static List<Task> taskList= new ArrayList<>();
+	public static Set<String> userNames = new HashSet<>();
+	public static Set<String> emails = new HashSet<>();
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
