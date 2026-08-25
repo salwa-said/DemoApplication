639fa711cf64043550d3b151b497ed9d90295230
salwa-said <salwwa2000@gmail.com>
2026-08-25 15:30:14 +0400

    Step 3 — Make TaskService

---
+++ demo/src/main/java/com/cl/demo/services/TaskService.java
@@ -0,0 +1,9 @@
+package com.cl.demo.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+        public static final String TASK_SAVED = "Task saved";
+}
+
