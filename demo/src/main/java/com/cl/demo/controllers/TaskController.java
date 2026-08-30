64d78a4e35ee36cde91afece6963c39d282e84b7
salwa-said <salwwa2000@gmail.com>
2026-08-30 08:31:01 +0400

    Step 4 — Fill in TaskController(POST /task/add )

--- demo/src/main/java/com/cl/demo/controllers/TaskController.java
+++ demo/src/main/java/com/cl/demo/controllers/TaskController.java
@@ -1,4 +1,26 @@
 package com.cl.demo.controllers;
 
-public class TaskController {
+import com.cl.demo.entities.Task;
+import com.cl.demo.requestobjects.TaskCreateRequest;
+import com.cl.demo.requestobjects.TaskUpdateRequest;
+import com.cl.demo.responseobjects.TaskCreateResponse;
+import com.cl.demo.responseobjects.TaskUpdateResponse;
+import com.cl.demo.services.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("task")
+
+public class TaskController {@Autowired
+public TaskService taskService;
+
+    @PostMapping("add")
+    public Map<String, String> addTask(@RequestBody TaskCreateRequest task) {
+        return taskService.addTask(task);
+    }
+
 }
