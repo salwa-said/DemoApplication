5ae1a90745839193a0bcc3b571de91b943ddb8d8
salwa-said <salwwa2000@gmail.com>
2026-08-30 09:03:42 +0400

    Step 4 — Make PhoneNumberController:POST /phoneNumber/add

---
+++ demo/src/main/java/com/cl/demo/controllers/PhoneNumberController.java
@@ -0,0 +1,19 @@
+package com.cl.demo.controllers;
+
+import com.cl.demo.entities.PhoneNumber;
+import com.cl.demo.requestobjects.PhoneNumberCreateRequest;
+import com.cl.demo.requestobjects.PhoneNumberUpdateRequest;
+import com.cl.demo.responseobjects.PhoneNumberCreateResponse;
+import com.cl.demo.responseobjects.PhoneNumberUpdateResponse;
+import com.cl.demo.services.PhoneNumberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("phoneNumber")
+public class PhoneNumberController {
+    @Autowired
+    public PhoneNumberService phoneNumberService;
+}
